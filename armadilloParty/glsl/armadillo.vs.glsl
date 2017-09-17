19e256c127e7021568841e77655a950e3445a3da
jc-chen <jean_21_chen@hotmail.com>
2017-09-17 13:55:59 -0700

    altered green light

--- armadilloParty/glsl/armadillo.vs.glsl
+++ armadilloParty/glsl/armadillo.vs.glsl
@@ -9,9 +9,12 @@ void main() {
     interpolatedNormal = modelMatrix * normal;
 
     // Set shared variable to vertex position
-    Position = modelMatrix * position;
 
     vec3 armadilloPos = vec3(1.5*sin(armadilloPosition.x),0.5*sin(3.*armadilloPosition.y)+0.5,armadilloPosition.z);
+   
+
+    Position = modelMatrix * (position+armadilloPos);
+
     // Multiply each vertex by the model-view matrix and the projection matrix to get final vertex position
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position+armadilloPos, 1.0);
 }
